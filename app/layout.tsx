f1006f7cd0abf0b9858b87cd0371412b13f92e18
AvasBrahma <avasbrahma@gmail.com>
2024-12-21 21:00:22 +0530

    added grid and aceternity ui floating button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+import { ThemeProvider } from "./provider";
+
+const inter=Inter({ subsets: ["latin"]});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +29,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+       <ThemeProvider
+       attribute="class"
+       defaultTheme="dark"
+       enableSystem
+       disableTransitionOnChange
+     >
+     {children}
+     </ThemeProvider>
       </body>
     </html>
   );
